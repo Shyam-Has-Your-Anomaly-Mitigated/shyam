18f939650b803bb8c286dd90734dadda94b4597a
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2018-10-24 02:51:20 +1100

    Add files via upload

---
+++ shyam_iso2usb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# purpose: self-documented
+# usage: sudo shyam_iso2usb.sh distro.iso /dev/sdX
+
+# tab complete
+file="$1.iso";
+file="$1";
+
+# gnome-disks
+device="/dev/sd$2";
+device="$2";
+
+# kitty >whiskers< nom 🙀🙀 sink!
+cat > "$device" < "$file" && sync;
+
+# esc(ape)
+exit;

f6b7636e672ae5fe4fa6ea62f0c3f228431439a8
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2023-01-21 16:29:35 +1100

    Update shyam_iso2usb.sh

--- shyam_iso2usb.sh
+++ shyam_iso2usb.sh
@@ -11,7 +11,9 @@ device="/dev/sd$2";
 device="$2";
 
 # kitty >whiskers< nom 🙀🙀 sink!
-time cat > "$device" < "$file" && sync; # 2.8g(2977595392)@6m1.032s && _
+time cat > "$device" < "$file" && time sync;
+# 2.8g(2977595392)@6m1.032s && _ @rpi
+# 2.8g(2977595392)@4m6.766s && 1m12.269s @2in1
 
 # esc(ape)
 exit;
